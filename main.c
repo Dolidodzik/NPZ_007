28f1f1f6675fef2c5d950f27fdaf457ca74bd3f5
S-Budzinski <szymonbudzinski1@gmail.com>
2025-01-20 11:12:43 +0100

    Done

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
+double stopnie_na_radiany(double stopnie) {
+    return stopnie * (180.0 / M_Pi);
+}
+
+int dzielenie_calkowite(int dzielna, int dzielnik) {
+    if (dzielnik == 0) {
+        printf("Błąd: Dzielenie przez zero!\n");
+        return 0; // Zwraca zero podczas bledu
+    }
+    return dzielna / dzielnik;
+}
 
 int main() 
 { 
-	printf("hello world");
-	return 0; 
+    printf("Hello World");
+    return 0;
 }
 
