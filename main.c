734c30acfe18e56a560b907b9a468fe4755e9156
Daniel <danielkudla2004@gmail.com>
2025-01-20 11:07:17 +0100

    Wykonanie funkcji mnozenie i dzielenie

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+double mnozenie(double a, double b) {
+    return a * b;
+}
+
+double dzielenie(double a, double b) {
+    if (b == 0) {
+        printf("Blad: dzielenie przez zero!\n");
+        return 0;
+    }
+    return a / b;
+}
+
 
 int main() 
-{ 
+{
 	printf("hello world");
 	return 0; 
 }
